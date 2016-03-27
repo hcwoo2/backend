96e795dd30827270a452b21e27e5b52ff14cd335
Frank  Syrek <franksyrek@Franks-MBP.attlocal.net>
2016-03-27 01:18:41 -0500

    created local server that returns nutrition upon button press

--- app/views.py
+++ app/views.py
@@ -1,8 +1,33 @@
 from app import app
+from flask import render_template, jsonify
 
-@app.route('/')
 
-app.route('/results')
+@app.route('/', methods= ['GET', 'POST'])
 
-def index():
-	
+def homepage():
+	return render_template('homepage.html')
+
+@app.route('/results') 
+
+def results():
+	my_results = {"calories": 550,
+					"fat cal": 110,
+					"fat GM": 5,
+					"fat %": 20,
+					"sat fat GM": 9999,
+					"sat fat %": 21,
+					'trans fat GM': 0,
+					"Chol MG": 0,
+					"Chol %": 0,
+					"sodium MG": 2,
+					"sodium %": 3,
+					"carbs GM": 77,
+					"carbs %": 3333,
+					"Dietary fiber GM": 6,
+					"Dietary fiber %": 7,
+					"sugars GM": 999,
+					"protien GM": 9900990,
+					"calcium %":10,
+					"iron %": 20
+					}
+	return jsonify(my_results)
